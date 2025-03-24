f20555aa4cbf1b20286a27a8881858e17c372983
root <karimhabouchi310@gmail.com>
2025-03-24 13:51:45 +0000

    base

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
